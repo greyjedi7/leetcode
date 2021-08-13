ed99740fdccce8ec3322a1498f0fde99f7cf5c61
greyjedi7 <karthigeyanmsd@gmail.com>
2021-08-13 22:25:32 +0530

    Set Matrix Zeros

--- 2021/Aug 8-14/Set Matrix Zeros/setZeros.cpp
+++ 2021/Aug 8-14/Set Matrix Zeros/setZeros.cpp
@@ -2,38 +2,85 @@
 using namespace std;
 
 
-void setZeros(vector<vector<int>>&  matrix){
+void print(vector<vector<int>> input){
+
+    
+
+    for(int i=0;i<input.size();i++){
+        for(int j=0;j<input[i].size();j++){
+            cout<<input[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void setZeroes(vector<vector<int>>&  matrix){
+
+    bool row = false;
+    bool col = false;
+
+    for(int i=0;i<matrix[0].size();i++){
+        if(matrix[0][i] == 0){
+            row = true;
+            break;
+        }
+    }
+
+    for(int i=0;i<matrix.size();i++){
+        if(matrix[i][0] == 0){
+            col = true;
+            break;
+        }
+    }
     for(int i=0;i<matrix.size();i++){
         for(int j=0;j<matrix[i].size();j++){
             if(matrix[i][j] == 0){
-                matrix[i][0] = -1;
-                matrix[0][j] = -1;
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
             }
         }
     }
 
     for(int i=1;i<matrix.size();i++){
-        if(matrix[i][0] == -1){
+        if(matrix[i][0] == 0){
             for(int j=0;j<matrix[i].size();j++){
                 matrix[i][j] = 0;
             }
         }
     }
 
-    for(int i=0;i<matrix[0].size();i++){
-        if(matrix[0][i] == -1){
-            for()
+    for(int i=1;i<matrix[0].size();i++){
+        if(matrix[0][i] == 0){
+            for(int j=0;j<matrix.size();j++){
+                matrix[j][i] = 0;
+            }
         }
     }
+
+    if(row){
+        for(int i=0;i<matrix[0].size();i++) matrix[0][i] = 0;
+    }
+
+    if(col){
+        for(int i=0;i<matrix.size();i++)   matrix[i][0] = 0;
+    }
+
 }
+
+
 
 int main(){
     
     vector<vector<int>> input {
-        {0,1,2,0},
-        {3,4,5,2},
-        {1,3,1,5}
+        {1,2,3,4},
+        {5,0,7,8},
+        {0,10,11,12},
+        {13,14,15,0}
     };
+
+    setZeroes(input);
+    
+    print(input);
 
     return 0;
 }
